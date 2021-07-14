82da166c1dc77b53ee3613222050fa21afbcb96d
Puneet Singh <er.puneet24@gmail.com>
2021-07-14 14:44:55 +0530

    Updated jwks jwt validate

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
-# node-sdk
-NodeJs sdk for MojoAuth passwordless authentication
+# Node-js-sdk
+Node.js SDK based on MojoAuth APIs.
+
+![Home Image](https://mojoauth-cdn.netlify.app/assets/images/coming-soon.png)
+
+
+## Introduction ##
+
+MojoAuth Node.js is a wrapper which provides access to MojoAuth Platform APIs.
+
+MojoAuth provides a secure and delightful experience to your customer with passwordless.
+Here, you'll find comprehensive guides and documentation to help you to start working with MojoAuth APIs.
+
+Please visit [here](http://www.mojoauth.com/) for more information.
+
+# Installing
+
+	$ npm install mojoauth-sdk
+
+
+## Documentation
+
+[Getting Started](https://mojoauth.com/docs/) - Everything you need to begin using this SDK.
---
+++ sdk/mojoauth-api.js
@@ -0,0 +1,91 @@
+/*
+ * Created by MojoAUth Development Team
+   Copyright 2021 MojoAuth. All rights reserved.
+*/
+module.exports = function (config) {
+
+  function signinWithMagicLink(email) {
+    var queryParameters = {};
+
+    queryParameters.api_Key = config.apiKey;
+
+    var bodyParameters = {};
+    bodyParameters.email = email;
+
+    var resourcePath = 'users/magiclink';
+
+    return config.request('POST', resourcePath, queryParameters, bodyParameters);
+  }
+
+  function verifyToken(token) {
+    var queryParameters = {};
+
+    queryParameters.api_Key = config.apiKey;
+
+    var resourcePath = 'token/jwks';
+
+    return new Promise(function (resolve, reject) {
+
+      var jwksPromise =  config.request('GET', resourcePath, queryParameters, "");
+
+      jwksPromise.then(function (jwksResponse) {
+        const jwktopem = require('jwk-to-pem');
+        const jwt = require('jsonwebtoken');
+        const [ firstKey ] = jwksResponse.keys
+        const publicKey = jwktopem(firstKey)
+        try {
+          const decoded = jwt.verify(token, publicKey)
+          resolve(decoded)
+        } catch (e) {
+          reject(e)
+        }
+      }).catch(function (error) {
+        reject(error)
+      });
+    });
+  }
+
+  function pingStaus(guid) {
+    var queryParameters = {};
+
+    queryParameters.api_Key = config.apiKey;
+    queryParameters.guid = guid;
+
+    var resourcePath = 'users/status';
+
+    return config.request('GET', resourcePath, queryParameters, "");
+  }
+
+  function signinWithEmailOTP(email) {
+    var queryParameters = {};
+
+    queryParameters.api_Key = config.apiKey;
+
+    var bodyParameters = {};
+    bodyParameters.email = email;
+
+    var resourcePath = 'users/emailotp';
+
+    return config.request('POST', resourcePath, queryParameters, bodyParameters);
+  }
+
+  function verifyOtp(otp) {
+    var queryParameters = {};
+
+    queryParameters.api_Key = config.apiKey;
+    queryParameters.otp = otp;
+
+    var resourcePath = 'users/emailotp/verify';
+
+    return config.request('GET', resourcePath, queryParameters, "");
+  }
+
+  return {
+    signinWithMagicLink: signinWithMagicLink,
+    verifyToken: verifyToken,
+    pingStaus: pingStaus,
+    signinWithEmailOTP: signinWithEmailOTP,
+    verifyOtp: verifyOtp
+  };
+}
+
