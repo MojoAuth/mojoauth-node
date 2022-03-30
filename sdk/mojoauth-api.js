6b51e43b14729006cff952c781e65754b67c76a7
Ty Mick <ty@tymick.me>
2022-03-30 07:37:11 -0700

    Stop sending API key in query parameters
    Per [your API docs][1], you only accept the API key in the request
    header, not in the query parameters. And `config.request` correctly
    [sets the `X-API-Key` header][2] anyway.
    
    [1]: https://mojoauth.com/docs/api/
    [2]: https://github.com/MojoAuth/mojoauth-node/blob/100e3a3545d0c025c42ed5f7b3ba6632c7715799/sdk/util/mojoauth.js#L50-L52

--- sdk/mojoauth-api.js
+++ sdk/mojoauth-api.js
@@ -5,28 +5,20 @@
 module.exports = function (config) {
 
   function signinWithMagicLink(email) {
-    var queryParameters = {};
-
-    queryParameters.api_Key = config.apiKey;
-
     var bodyParameters = {};
     bodyParameters.email = email;
 
     var resourcePath = 'users/magiclink';
 
-    return config.request('POST', resourcePath, queryParameters, bodyParameters);
+    return config.request('POST', resourcePath, "", bodyParameters);
   }
 
   function verifyToken(token) {
-    var queryParameters = {};
-
-    queryParameters.api_Key = config.apiKey;
-
     var resourcePath = 'token/jwks';
 
     return new Promise(function (resolve, reject) {
 
-      var jwksPromise =  config.request('GET', resourcePath, queryParameters, "");
+      var jwksPromise =  config.request('GET', resourcePath, "", "");
 
       jwksPromise.then(function (jwksResponse) {
         const jwktopem = require('jwk-to-pem');
@@ -50,8 +42,6 @@ module.exports = function (config) {
 
   function pingStaus(guid) {
     var queryParameters = {};
-
-    queryParameters.api_Key = config.apiKey;
     queryParameters.guid = guid;
 
     var resourcePath = 'users/status';
@@ -60,22 +50,16 @@ module.exports = function (config) {
   }
 
   function signinWithEmailOTP(email) {
-    var queryParameters = {};
-
-    queryParameters.api_Key = config.apiKey;
-
     var bodyParameters = {};
     bodyParameters.email = email;
 
     var resourcePath = 'users/emailotp';
 
-    return config.request('POST', resourcePath, queryParameters, bodyParameters);
+    return config.request('POST', resourcePath, "", bodyParameters);
   }
 
   function verifyOtp(otp) {
     var queryParameters = {};
-
-    queryParameters.api_Key = config.apiKey;
     queryParameters.otp = otp;
 
     var resourcePath = 'users/emailotp/verify';
